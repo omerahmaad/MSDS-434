d1d19ba49b19651f54d8762cd8479931c6918489
omerahmaad <omerahmaad@gmail.com>
2019-04-14 09:55:03 -0500

    Update test_myrepo.py

--- test/test_myrepo.py
+++ test/test_myrepo.py
@@ -4,3 +4,7 @@ from myrepolib import repomod
 def test_func():
     result = repomod.myfunc()
     assert result == 9
+    
+def test_addition():
+    result = repomod.addition(2,2)
+    assert result = 4
